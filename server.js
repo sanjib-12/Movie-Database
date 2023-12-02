8a009fbf014c04c93aeee27258a44d18dd0a052f
sanjib <sanjibbarakoti112@gmail.com>
2023-12-02 17:31:30 -0500

    Creating Schema and Model : server.js

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});//this should always come before any import.
 
 const app = require('./app');
 
-console.log(process.env);
+//console.log(process.env);
+
+mongoose.connect(process.env.CONN_STR)
+.then((conn)=>{    
+    //console.log(conn);
+    console.log('DB connection successfull');
+}).catch((error) =>{
+    console.log('error in connection.')
+})
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        requried: [true,'Name is required field!'],
+        unique: true
+    },
+    description: String,
+    duration: {
+        type: Number,
+        required: [true,'duration is required field!']
+    },
+    ratings: {
+        type: Number,
+        default: 0.0
+    },
+});
+
+//const movie = mongoose.model('movie','movieSchema');
 
 const port = process.env.PORT || 3000;
 
@@ -11,6 +39,3 @@ app.listen(port,() =>{
     console.log('Server has started...');
 })
 
-//we can access environment variable form all the file using the process.env becasue process
-// is sync to all no matter whether we are in which file. 
-//dotenv.config({path: './config.env'}) this allows us to access the process for config.env files.
