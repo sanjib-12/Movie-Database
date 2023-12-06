3f343f58ded72a1a0f3f384f9e84b75115388350
sanjib <sanjibbarakoti112@gmail.com>
2023-12-06 14:28:35 -0500

    Default route

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ if(process.env.NODE_ENV === 'development'){
 app.use(express.static('./public/'))
 
 app.use('/api/v1/movies', moviesRouter);
+//the below url will apply to all the url which do not match the proper route.
+//it should be always defiend at the last so that it doesn't execute first.
+app.all("*",(req, res, next)=>{
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on the server`
+    });
+    next();
+});
 
 module.exports = app;
 
