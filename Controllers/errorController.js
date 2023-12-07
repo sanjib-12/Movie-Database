889bfcf5b1c144814df1d8ef06c6f5a7773c277f
sanjib <sanjibbarakoti112@gmail.com>
2023-12-07 02:08:08 -0500

    Development and Production Error

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,9 +1,28 @@
-module.exports = (error, req, res, next) =>{
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'error';
+const devErrors=(res, error) =>{
+    res.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message,
+        stackTree: error.stack,
+        error: error
+    });
+}
+const prodErrors=(res, error) =>{
     res.status(error.statusCode).json({
         status: error.statusCode,
         message: error.message
-    })
-    next();
+    });
+}
+
+
+module.exports = (error, req, res, next) =>{
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+    
+
+    if(process.env.NODE_ENV === 'development'){
+        devErrors(res, error);
+    }else if(process.env.NODE_ENV === 'production'){
+        prodErrors(res, error);
+    }
+    //next();
 };
