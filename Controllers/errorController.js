ef4e5dc618b75007f0239098f160c01d47df093c
sanjib <sanjibbarakoti112@gmail.com>
2023-12-07 15:09:35 -0500

    Handeling Duplicate key error

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -26,6 +26,12 @@ const castErrorHandler=(err)=>{
     const msg = `Invalid value for ${err.path}: ${err.value}!`;
     return new CustomError(msg, 400)
 }
+
+const duplicateKeyErrorHandler = (err) =>{
+    const name = err.keyValue.name;
+    const msg = `There is already a movie with name '${name}'.Please use another name!`;
+    return new CustomError(msg, 400);
+}
     
 
 
@@ -37,11 +43,15 @@ module.exports = (error, req, res, next) =>{
     if(process.env.NODE_ENV === 'development'){
         devErrors(res, error);
     }else if(process.env.NODE_ENV === 'production'){
-        
-        
+        console.log(error)
+
         if(error.name === 'CastError'){
             
             error = castErrorHandler(error);
+        }
+        if(error.code === 11000){
+            
+            error = duplicateKeyErrorHandler(error);
         }
         prodErrors(res, error);
     }
