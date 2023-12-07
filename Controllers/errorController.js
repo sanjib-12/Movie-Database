3306d37ce495836d3fe522718a03c63fbd8c82c1
sanjib <sanjibbarakoti112@gmail.com>
2023-12-07 14:41:35 -0500

    Handeling invalid Id

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,3 +1,5 @@
+const CustomError = require('./../Utils/CustomError');
+
 const devErrors=(res, error) =>{
     res.status(error.statusCode).json({
         status: error.statusCode,
@@ -7,11 +9,24 @@ const devErrors=(res, error) =>{
     });
 }
 const prodErrors=(res, error) =>{
-    res.status(error.statusCode).json({
-        status: error.statusCode,
-        message: error.message
-    });
+    if(error.isOperational){
+        res.status(error.statusCode).json({
+            status: error.statusCode,
+            message: error.message
+        });
+    }else {
+        res.status(500).json({
+            status: 'error',
+            message: 'something went wrong please try again!'
+        })
+    }
 }
+
+const castErrorHandler=(err)=>{
+    const msg = `Invalid value for ${err.path}: ${err.value}!`;
+    return new CustomError(msg, 400)
+}
+    
 
 
 module.exports = (error, req, res, next) =>{
@@ -22,6 +37,12 @@ module.exports = (error, req, res, next) =>{
     if(process.env.NODE_ENV === 'development'){
         devErrors(res, error);
     }else if(process.env.NODE_ENV === 'production'){
+        
+        
+        if(error.name === 'CastError'){
+            
+            error = castErrorHandler(error);
+        }
         prodErrors(res, error);
     }
     //next();
